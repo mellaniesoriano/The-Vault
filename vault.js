a4b51ee1aed3d5a843a05695b505b334adcf849a
mellaniesoriano <msoriano808@gmail.com>
2017-05-18 19:49:03 -1000

    tests pass

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+
+var vaultObj = {};
+
+function setValue(key, value){
+  vaultObj[key] = value;
+  return key;
+}
+
+function getValue(key){
+  if(vaultObj.hasOwnProperty(key)){
+    return vaultObj[key];
+  } else {
+    return null;
+  }
+}
+
+return {
+  setValue: setValue,
+  getValue: getValue
+};
+
 };
